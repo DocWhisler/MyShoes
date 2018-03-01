f36662523b16ca84e191de1795004064625f515b
Benjamin Kriese <b.kriese@tup.com>
2018-03-01 19:05:33 +0100

    PopupWindow v2.0

--- app/src/main/java/com/tech/oma/myshoes/MainActivity.java
+++ app/src/main/java/com/tech/oma/myshoes/MainActivity.java
@@ -5,15 +5,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
@@ -24,30 +27,46 @@ public class MainActivity extends AppCompatActivity {
 
     private ArrayList<Shoe> shoes ;
     private Context mContext;
+    private CoordinatorLayout mCoordianteLayout;
+    private FloatingActionButton fab;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.mContext = this.getApplicationContext();
+        this.mCoordianteLayout = (CoordinatorLayout)findViewById(R.id.mainactivity_layout);
+
         // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         this.setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add);
+        this.fab = (FloatingActionButton) findViewById(R.id.add);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
             // Android PopUp Window
-            startActivity(new Intent(MainActivity.this, PopUpWindow_Activity.class));
+            inflater = (LayoutInflater)mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+            ViewGroup container = (ViewGroup) inflater.inflate(R.layout.popupwindow__layout, null);
 
+            final PopupWindow popupWindow = new PopupWindow(container, 400, 400, true);
+            popupWindow.showAtLocation(mCoordianteLayout, Gravity., 500, 500);
+
+            container.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    popupWindow.dismiss();
+                    return true;
+                }
+            });
 
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
             }
         });
-
     }
 
     @Override
