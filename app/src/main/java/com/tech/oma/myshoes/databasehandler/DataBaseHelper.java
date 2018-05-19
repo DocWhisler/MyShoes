c6559ac6a5616eb061c21371bbc7b2de6635fecb
=OMA= Whisler <doc_whisler@yahoo.de>
2018-05-19 16:29:54 +0200

    DataBaseHelper finished

--- app/src/main/java/com/tech/oma/myshoes/databasehandler/DataBaseHelper.java
+++ app/src/main/java/com/tech/oma/myshoes/databasehandler/DataBaseHelper.java
@@ -1,11 +1,15 @@
 package com.tech.oma.myshoes.databasehandler;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DataBaseHelper extends SQLiteOpenHelper {
 
     // http://androidopentutorials.com/android-sqlite-join-multiple-tables-example/
+    // contentValues.put("yourDateColumn", date.getTime());
+    // new Date(cursor.getLong(yourColumnIndex));
+    //https://stackoverflow.com/questions/20495083/android-sqlite-store-date-types
 
     // All Static variables
     // Database Version
@@ -16,30 +20,90 @@ public class DataBaseHelper extends SQLiteOpenHelper {
 
     // Shoe table name
     private static final String TABLE_SHOES = "shoes";
+    private static final String TABLE_LISTS = "lists";
+    private static final String TABLE_SHOE2LISTS = "shoe2lists";
 
     // Shoe Table Columns names
-    private static final String KEY_OID = "oid";
-    private static final String KEY_ID = "id";
-    private static final String KEY_TITEL = "titel";
-    private static final String KEY_DESCRIPTION = "description";
-    private static final String KEY_IMAGEPATH = "imagePath";
-    private static final String KEY_ART = "art";
-    private static final String KEY_PRICE = "price";
-    private static final String DBEXCEPTION = "Database error";
+    private static final String SHOE_OID = "oid";
+    private static final String SHOE_ID = "id";
+    private static final String SHOE_TITEL = "titel";
+    private static final String SHOE_DESCRIPTION = "description";
+    private static final String SHOE_IMAGEPATH = "imagePath";
+    private static final String SHOE_TAG = "tag";
+    private static final String SHOE_PRICE = "price";
+    private static final String SHOE_CREATED = "created";
 
-    private static ShoeDBHandlerImpl instance;
-    private static SQLiteDatabase readableDatabase;
-    private static SQLiteDatabase writableDatabase;
+    // Lists Table
+    private static final String LISTS_OID = "oid";
+    private static final String LISTS_ID = "id";
+    private static final String LISTS_NAME = "name";
+    private static final String LISTS_CREATED = "created";
 
+    // Shoe2Lists Table
+    private static final String SHOE2LISTS_OID = "oid";
+    private static final String SHOE2LISTS_SHOE = "shoe";
+    private static final String SHOE2LISTS_LIST = "list";
+    private static final String SHOE2LISTS_CREATED = "created";
 
+    // Create Tables
+    private static final String CREATE_SHOE_TABLE = "CREATE TABLE " + TABLE_SHOES + "("
+            + SHOE_OID + " TEXT PRIMARY KEY,"
+            + SHOE_ID + " INTEGER,"
+            + SHOE_TITEL + " TEXT,"
+            + SHOE_DESCRIPTION + " TEXT,"
+            + SHOE_IMAGEPATH + " TEXT,"
+            + SHOE_TAG + " TEXT,"
+            + SHOE_PRICE + " REAL,"
+            + SHOE_CREATED + " INTEGER)";
+
+    private static final String CREATE_LIST_TABLE = "CREATE TABLE " + TABLE_LISTS + "("
+            + LISTS_OID + " TEXT PRIMARY KEY,"
+            + LISTS_ID + " INTEGER,"
+            + LISTS_NAME + " TEXT,"
+            + LISTS_CREATED + " INTEGER)";
+
+    private static final String CREATE_SHOE2LIST_TABLE = "CREATE TABLE " + TABLE_SHOE2LISTS + "("
+            + SHOE2LISTS_OID + " TEXT PRIMARY KEY,"
+            + SHOE2LISTS_SHOE + " TEXT,"
+            + SHOE2LISTS_LIST + " TEXT,"
+            + SHOE2LISTS_CREATED + " INTEGER)";
+
+    private static DataBaseHelper instance;
+
+    private DataBaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    public static synchronized DataBaseHelper getDbHelperInstance(Context context) {
+        if (instance == null)
+            instance = new DataBaseHelper(context);
+        return instance;
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        if (!db.isReadOnly()) {
+            // Enable foreign key constraints
+            db.execSQL("PRAGMA foreign_keys=ON;");
+        }
+    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_SHOE_TABLE);
+        db.execSQL(CREATE_LIST_TABLE);
+        db.execSQL(CREATE_SHOE2LIST_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOES);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_LISTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_SHOE2LISTS);
 
+        // Create tables again
+        onCreate(db);
     }
 }
